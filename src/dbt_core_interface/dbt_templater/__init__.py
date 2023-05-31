d7d50ecda6a2d27a28066f22a3fdc97dd18b4ad0
Barry Hart <barry.hart@zoro.com>
2023-05-31 14:01:55 -0400

    PR review

--- src/dbt_core_interface/dbt_templater/__init__.py
+++ src/dbt_core_interface/dbt_templater/__init__.py
@@ -1,13 +1,24 @@
 """Defines the hook endpoints for the dbt templater plugin."""
+import importlib
+import logging
+import sys
+
 from sqlfluff.core.plugin import hookimpl
 
-#import dbt_osmosis.core.server_v2
 from dbt_core_interface.dbt_templater.templater import OsmosisDbtTemplater
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 @hookimpl
 def get_templaters():
     """Get templaters."""
+    if importlib.util.find_spec('sqlfluff_templater_dbt'):
+        LOGGER.error("sqlfluff-templater-dbt is not compatible with dbt-osmosis server. "
+                       "Please uninstall it to continue.")
+        sys.exit(1)
+
 
     def create_templater(**kwargs):
         import dbt_core_interface.state
