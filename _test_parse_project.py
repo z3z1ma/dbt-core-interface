f778426da84eeb98cc000a78dbe4127f32ee5622
z3z1ma <butler.alex2010@gmail.com>
2023-06-04 00:48:40 -0700

    feat: accept param inputs in register for parity with prior impl

--- _test_parse_project.py
+++ _test_parse_project.py
@@ -3,4 +3,6 @@ from dbt_core_interface import DbtProject
 
 if __name__ == "__main__":
     project = DbtProject()
-    print(project.list("*"))
+    for i in project.list("source:showpad+"):
+        print(i)
+    print(bool(project))
