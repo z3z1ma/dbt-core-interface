550a613dd95cf3b341d949c3304ee93a3c1e6e7d
Bruno Antonellini <antonellinibruno@gmail.com>
2024-11-07 15:32:45 -0300

    Ignore Jinja % tags when compiling (#12)

--- src/dbt_core_interface/project.py
+++ src/dbt_core_interface/project.py
@@ -703,6 +703,8 @@ class DbtProject:
 
         This is a context manager that will clear the node after execution and leverages a mutex during manifest mutation.
         """
+        # Remove {% ... %} patterns from the SQL string
+        sql = re.sub(r'{%.*?%}', '', sql, flags=re.DOTALL)
         with self.manifest_mutation_mutex:
             self._clear_node(node_name)
             sql_node = self.sql_parser.parse_remote(sql, node_name)
